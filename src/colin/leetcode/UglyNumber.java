a026b12ef7c11d1f5a8707c7f0a1c5f983f22f56
Colin Wang <260790736@qq.com>
2016-03-04 15:54:00 +0800

    Add sulotion for ugly number.

---
+++ src/colin/leetcode/UglyNumber.java
@@ -0,0 +1,30 @@
+package colin.leetcode;
+
+/**
+ * Write a program to check whether a given number is an ugly number.
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ * For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+ * Note that 1 is typically treated as an ugly number.
+ * Created by colin on 3/4/16.
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+
+        while (num % 5 == 0) {
+            num = num / 5;
+        }
+
+        while (num % 3 == 0) {
+            num = num / 3;
+        }
+
+        while (num % 2 == 0) {
+            num = num / 2;
+        }
+
+        return num == 1;
+    }
+}
