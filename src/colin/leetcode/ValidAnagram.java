c0c6b1400ad664988168e258b8a4822d076ab374
Colin Wang <260790736@qq.com>
2016-03-07 14:25:15 +0800

    Valid Anagram

---
+++ src/colin/leetcode/ValidAnagram.java
@@ -0,0 +1,30 @@
+package colin.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Given two strings s and t, write a function to determine if t is an anagram of s.
+ *
+ * For example,
+ * s = "anagram", t = "nagaram", return true.
+ * s = "rat", t = "car", return false.
+ *
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ * Created by colin on 3/7/16.
+ */
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars,tChars);
+    }
+}
